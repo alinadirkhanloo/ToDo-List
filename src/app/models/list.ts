02ac1e8375ef3eb1ac1c2fec4a2959099d99488d
alinadir <alinadirkhanlo.98@gmail.com>
2024-09-18 14:33:26 +0330

    Create Form models

---
+++ src/app/models/list.ts
@@ -0,0 +1,6 @@
+export interface ToDoList {
+  _id: string;
+  title: string;
+  date: Date;
+  isMain: boolean;
+}
---
+++ src/app/models/task.ts
@@ -0,0 +1,8 @@
+export interface ToDoTask {
+  _id?: string;
+  title: string;
+  date: Date;
+  description: string;
+  done: boolean;
+  list: string;
+}
